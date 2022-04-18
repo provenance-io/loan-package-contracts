fd2b5733530f07d9f51eb1f56dd9d6f4064dfd59
Ravi Patel <rpatel@figure.com>
2022-04-18 11:17:47 -0500

    Add TODO on metadata asset model prerelease artifact

--- contract/build.gradle.kts
+++ contract/build.gradle.kts
@@ -12,7 +12,7 @@ dependencies {
     api(project(":proto"))
     implementation("io.provenance.scope:contract-base:0.4.9")
     implementation("io.provenance.scope:util:0.4.9")
-    implementation("io.provenance.model:metadata-asset-model:loan-wrapper") {
+    implementation("io.provenance.model:metadata-asset-model:loan-wrapper") { // TODO: Change from prerelease artifact
         exclude("com.google.protobuf", "protobuf-java")
         exclude("com.google.protobuf", "protobuf-java-util")
         exclude("io.grpc", "grpc-protobuf")
