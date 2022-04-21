aaf2e2a765b5e9acade66b2b43888b3f1cd8b315
Cody Worsnop <cworsnop@figure.com>
2022-04-21 10:37:47 -0700

    fixing invalid pom rule for publishing

--- contract/build.gradle.kts
+++ contract/build.gradle.kts
@@ -14,7 +14,7 @@ dependencies {
     api(project(":proto"))
     implementation("io.provenance.scope:contract-base:0.4.9")
     implementation("io.provenance.scope:util:0.4.9")
-    implementation("io.provenance.model:metadata-asset-model:0.1.+") {
+    implementation("io.provenance.model:metadata-asset-model:0.1.6") {
         exclude("com.google.protobuf", "protobuf-java")
         exclude("com.google.protobuf", "protobuf-java-util")
         exclude("io.grpc", "grpc-protobuf")
